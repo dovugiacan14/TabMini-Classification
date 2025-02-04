4a3304832a6a0480131e5815590f193db208784e
can <can.dvg@softworld.jp>
2025-02-04 14:10:00 +0700

    add train test split

--- tabmini/estimators/XGBoost.py
+++ tabmini/estimators/XGBoost.py
@@ -3,7 +3,7 @@ import pandas as pd
 from xgboost import XGBClassifier
 
 from sklearn.utils import check_X_y
-from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.utils.validation import check_is_fitted, check_array
 from sklearn.base import BaseEstimator, ClassifierMixin
@@ -32,16 +32,23 @@ class XGBoost(BaseEstimator, ClassifierMixin):
 
     def fit(self, X, y) -> "XGBoost":
         X, y = check_X_y(X, y, accept_sparse=True)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
         results = []
+        best_f1 = -1
+        best_model = None
+
         param_combinations = [
             dict(zip(self.param_grid.keys(), v))
             for v in np.array(np.meshgrid(*self.param_grid.values())).T.reshape(
                 -1, len(self.param_grid.keys())
             )
         ]
+
         for param in param_combinations:
             param["n_estimators"] = int(param["n_estimators"])
             param["max_depth"] = int(param["max_depth"])
+
             current_model = XGBClassifier(
                 **param,
                 objective="binary:logistic",
@@ -49,16 +56,25 @@ class XGBoost(BaseEstimator, ClassifierMixin):
                 use_label_encoder=False,
                 random_state=self.seed
             )
-            current_model.fit(X, y)
+            current_model.fit(X_train, y_train)
 
             # make predictions
-            y_pred = current_model.predict(X)
-            f1 = f1_score(y, y_pred, average="binary")
-            acc = accuracy_score(y, y_pred)
+            y_pred = current_model.predict(X_test)
+            f1 = f1_score(y_test, y_pred, average="binary")
+            acc = accuracy_score(y_test, y_pred)
 
             results.append({**param, "accuracy": acc, "f1_score": f1})
 
-        self.result_df = pd.DataFrame(results)
+            # Update best model
+            if f1 > best_f1:
+                best_f1 = f1
+                best_model = current_model
+
+        self.result_df = pd.DataFrame(results).sort_values(
+            by="f1_score", ascending=False
+        )
+        self.model = best_model
+        self.best_params_ = best_model.get_params() if best_model else None
 
     def predict_proba(self, X):
         check_is_fitted(self)
