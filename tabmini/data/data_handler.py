e94d155ae3dcc26900d3d59f19ed9a70c57fdfb0
Windows-key <giacanemail@gmail.com>
2025-03-02 07:38:09 +0700

    fix bugs: error in downloads file

--- tabmini/data/data_handler.py
+++ tabmini/data/data_handler.py
@@ -1,9 +1,83 @@
+import os 
+import requests
 import pandas as pd
-from pmlb import fetch_data
 
 from tabmini.data import data_info
 from tabmini.types import TabminiDataset
 
+GITHUB_URL = 'https://github.com/EpistasisLab/penn-ml-benchmarks/raw/master/datasets'
+suffix = '.tsv.gz'
+
+
+def get_dataset_url(GITHUB_URL, dataset_name, suffix):
+    dataset_url = '{GITHUB_URL}/{DATASET_NAME}/{DATASET_NAME}{SUFFIX}'.format(
+                                GITHUB_URL=GITHUB_URL,
+                                DATASET_NAME=dataset_name,
+                                SUFFIX=suffix
+                                )
+
+    re = requests.get(dataset_url)
+    if re.status_code != 200:
+        raise ValueError('Dataset not found in PMLB.')
+    return dataset_url
+
+
+def fetch_data(dataset_name, return_X_y=False, local_cache_dir=None, dropna=True):
+    """Download a data set from the PMLB, (optionally) store it locally, and return the data set.
+
+    You must be connected to the internet if you are fetching a data set that is not cached locally.
+
+    Parameters
+    ----------
+    dataset_name: str
+        The name of the data set to load from PMLB.
+    return_X_y: bool (default: False)
+        Whether to return the data in scikit-learn format, with the features 
+        and labels stored in separate NumPy arrays.
+    local_cache_dir: str (default: None)
+        The directory on your local machine to store the data files.
+        If None, then the local data cache will not be used.
+    dropna: bool
+        If True, pmlb will drop NAs in exported dataset.
+
+    Returns
+    ----------
+    dataset: pd.DataFrame or (array-like, array-like)
+        if return_X_y == False: A pandas DataFrame containing the fetched data set.
+        if return_X_y == True: A tuple of NumPy arrays containing (features, labels)
+
+    """
+
+    if local_cache_dir is None:
+        dataset_url = get_dataset_url(GITHUB_URL,
+                                        dataset_name, suffix)
+        dataset = pd.read_csv(dataset_url, sep='\t', compression='gzip')
+    else:
+        dataset_path = os.path.join(local_cache_dir, dataset_name,
+                                    dataset_name+suffix)
+
+        # Use the local cache if the file already exists there
+        if os.path.exists(dataset_path):
+            dataset = pd.read_csv(dataset_path, sep='\t', compression='gzip')
+        # Download the data to the local cache if it is not already there
+        else:
+            dataset_url = get_dataset_url(GITHUB_URL,
+                                            dataset_name, suffix)
+            dataset = pd.read_csv(dataset_url, sep='\t', compression='gzip')
+            dataset_dir = os.path.split(dataset_path)[0]
+            if not os.path.isdir(dataset_dir):
+                os.makedirs(dataset_dir)
+            dataset.to_csv(dataset_path, sep='\t', compression='gzip',
+                    index=False)
+
+    if dropna:
+        dataset.dropna(inplace=True)
+    if return_X_y:
+        X = dataset.drop('target', axis=1).values
+        y = dataset['target'].values
+        return (X, y)
+    else:
+        return dataset
 
 def load_dataset(reduced: bool = False) -> TabminiDataset:
     """
@@ -19,7 +93,12 @@ def load_dataset(reduced: bool = False) -> TabminiDataset:
         datasets = _datasets if not reduced else [file for file in _datasets if data_info.is_not_excluded(file)]
 
         for dataset_name in datasets:
-            fetched_data = fetch_data(dataset_name)
+            try: 
+                fetched_data = fetch_data(dataset_name)
+            except: 
+                new_dataset_name = "_deprecated_" + dataset_name
+                fetched_data = fetch_data(new_dataset_name)
+
             if not isinstance(fetched_data, pd.DataFrame):
                 print(f"Dataset {dataset_name} is not an instance of DataFrame. Skipping...")
                 continue
